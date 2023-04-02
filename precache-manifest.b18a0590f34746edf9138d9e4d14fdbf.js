8ca54ff63009cf1782c610039a6feeb7d991c22b
sparksflyupwards <saad.jamil.khan@gmail.com>
2023-04-02 02:37:26 -0400

    Updates

---
+++ precache-manifest.b18a0590f34746edf9138d9e4d14fdbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "05bb3f7981133e76d802",
+    "url": "/Elements/static/css/main.1a364434.chunk.css"
+  },
+  {
+    "revision": "05bb3f7981133e76d802",
+    "url": "/Elements/static/js/main.05bb3f79.chunk.js"
+  },
+  {
+    "revision": "bfa17f515437437af62a",
+    "url": "/Elements/static/js/1.bfa17f51.chunk.js"
+  },
+  {
+    "revision": "91173f742aca7563cc9d",
+    "url": "/Elements/static/js/2.91173f74.chunk.js"
+  },
+  {
+    "revision": "6467a5d2b4880cadfdea",
+    "url": "/Elements/static/js/runtime~main.6467a5d2.js"
+  },
+  {
+    "revision": "d1848d758aad855a5f11e0fbce0c4039",
+    "url": "/Elements/static/media/man1.d1848d75.jpg"
+  },
+  {
+    "revision": "e4671bc52a37da0cb5ba73102c8ed1b1",
+    "url": "/Elements/static/media/man2.e4671bc5.jpg"
+  },
+  {
+    "revision": "2508a3a7c2cf35830ee95faa1811deec",
+    "url": "/Elements/static/media/woman1.2508a3a7.jpg"
+  },
+  {
+    "revision": "58017ec5c4e32607b935f77b8833fda4",
+    "url": "/Elements/index.html"
+  }
+];
